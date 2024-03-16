56a80a8bb63cc833df3cc16b9f494ca148b96f1f
mohammad <94738811+Mohamadkhosravi@users.noreply.github.com>
2024-03-16 14:29:20 +0330

    DEV:021226:board tested, and the IR threshold was set to 10 for fire mode

--- src/main.cpp
+++ src/main.cpp
@@ -37,13 +37,14 @@ void PrintVector(float *vData, uint16_t bufferSize, uint8_t scaleType);
 #define LUX_CALC_SCALAR           12518931
 #define LUX_CALC_EXPONENT         -1.405
 bool FIER=false;
+unsigned int FIERCounter=0;
 void setup()
 {
    // watchdog.enable(Watchdog::TIMEOUT_2S);
     pinMode(LED, OUTPUT);
     pinMode(LDR_CONNECT, OUTPUT);
     pinMode(NTC_CONNECT, OUTPUT);
-    Serial.begin(9600); 
+    // Serial.begin(9600); 
     //===============
     sampling_period_us = round(1000000*(1.0/samplingFrequency));
 
@@ -51,66 +52,86 @@ void setup()
 String LastPIR;
 void loop()
 {
- // watchdog.tripped();
-    double celsius = temperature(analogRead(ADC_NTC),5);
-    double VLDR = CALCULATE_VLDR(analogRead(ADC_LDR));
-    double RLDR = CALCULATE_RLDR(VLDR,10000);
-    double LUX= LUX_CALC_SCALAR * pow(RLDR, LUX_CALC_EXPONENT);
+
+    digitalWrite(LED, LOW);
+    lowPowerBoard.powerDown(SLEEP_2S,ADC_OFF, BOD_OFF);
+    // watchdog.tripped();
+    //  while (1)
+    //  {
+    // double celsius = temperature(analogRead(ADC_NTC),5);
+    // double VLDR = CALCULATE_VLDR(analogRead(ADC_LDR));
+    // double RLDR = CALCULATE_RLDR(VLDR,10000);
+    // double LUX= LUX_CALC_SCALAR * pow(RLDR, LUX_CALC_EXPONENT);
     // int PIR =analogRead(ADC_PIR);
-    double PIR;
+    // double PIR;
     int IR =analogRead(ADC_IR);
-    digitalWrite(LDR_CONNECT, LOW);
-    digitalWrite(NTC_CONNECT, LOW);
+    // digitalWrite(LDR_CONNECT, LOW);
+    // digitalWrite(NTC_CONNECT, LOW);
 
-  /*SAMPLING*/
-  microseconds = micros();
-  for(int i=0; i<samples; i++)
-  {
-      vReal[i] = analogRead(CHANNEL);
-      vImag[i] = 0;
-      while(micros() - microseconds < sampling_period_us){
-        //empty loop
+    /*SAMPLING*/
+    //  microseconds = micros();
+    //   for(int i=0; i<samples; i++)
+    //   {
+    //       vReal[i] = analogRead(CHANNEL);
+    //       vImag[i] = 0;
+    //       while(micros() - microseconds < sampling_period_us){
+    //         //empty loop
+    //       }
+    //       microseconds += sampling_period_us;
+    //   }
+
+    //     FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);	/* Weigh data */
+    //     FFT.compute(FFTDirection::Forward); /* Compute FFT */
+    //     FFT.complexToMagnitude(); /* Compute magnitudes */
+    //     PIR= FFT.majorPeak();
+    //     delay(100); /* Repeat after delay */
+    //   PIR=PIR*10.00;
+    // if (PIR<0)PIR=0;
+    // if (PIR>2000)PIR=1024;
+
+    // String SPTR =(String)PIR;
+    // if(SPTR==" NAN")SPTR=LastPIR;
+    // else LastPIR=SPTR;
+
+    // char buffer[3];
+    // dtostrf(PIR, 2, 3, buffer); 
+
+    //     String Data = 
+    //     "PIR="+(String)PIR
+    //     +"  IR="+(String)IR
+    //     +"  LDR="+(String)analogRead(ADC_LDR)
+    //     +"   LUX="+(String)LUX
+    //     +"   Temp="+(String)celsius;
+    //    Serial.println(Data);
+    // Serial.print( "PIR="+SPTR);
+    // Serial.print( "PIR="+PIR);
+    // Serial.print("  IR="+(String)IR);
+    // Serial.print("  LDR="+(String)analogRead(ADC_LDR));
+    // Serial.print("  LUX="+(String)LUX);
+    // Serial.print("  Temp="+(String)celsius);
+    // Serial.print("\n");
+    if (IR<10){
+      FIER=true;
+      FIERCounter++;
+    }
+    else {
+      FIER = false;
+      if(FIERCounter>0) FIERCounter--;
+    }
+
+    if(FIERCounter>3)
+    {
+      while (1)
+      {
+        digitalWrite(LED, HIGH);
       }
-      microseconds += sampling_period_us;
-  }
-
-    FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);	/* Weigh data */
-    FFT.compute(FFTDirection::Forward); /* Compute FFT */
-    FFT.complexToMagnitude(); /* Compute magnitudes */
-    PIR= FFT.majorPeak();
-    delay(100); /* Repeat after delay */
-  PIR=PIR*10.00;
-if (PIR<0)PIR=0;
-if (PIR>2000)PIR=1024;
-
-String SPTR =(String)PIR;
-if(SPTR==" NAN")SPTR=LastPIR;
-else LastPIR=SPTR;
-
-// char buffer[3];
-// dtostrf(PIR, 2, 3, buffer); 
-
-//     String Data = 
-//     "PIR="+(String)PIR
-//     +"  IR="+(String)IR
-//     +"  LDR="+(String)analogRead(ADC_LDR)
-//     +"   LUX="+(String)LUX
-//     +"   Temp="+(String)celsius;
-//    Serial.println(Data);
-Serial.print( "PIR="+SPTR);
-Serial.print("  IR="+(String)IR);
-Serial.print("  LDR="+(String)analogRead(ADC_LDR));
-Serial.print("  LUX="+(String)LUX);
-Serial.print("  Temp="+(String)celsius);
-Serial.print("\n");
-
-   if (IR<40)FIER=true;
-   else FIER = false;
-   digitalWrite(LED, FIER);
-//    delay(200);
-digitalWrite(LED, HIGH);
-    lowPowerBoard.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+    }
+     delay(100);
+    // if(micros() - microseconds < 200)break;
+    // lowPowerBoard.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+//  }
+ 
    
-lowPowerBoard.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF); 
+
 
 }
