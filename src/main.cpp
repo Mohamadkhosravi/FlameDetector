1043f4016e9f0c60f4342fb27d177bbb8a5a439b
mohammad <94738811+Mohamadkhosravi@users.noreply.github.com>
2024-04-14 17:16:39 +0330

    DEV:030126:read line power voltage

--- src/main.cpp
+++ src/main.cpp
@@ -1,132 +1,178 @@
 #include <Arduino.h>
 #include <main.hpp>
+#include <avr/power.h>
 
 Watchdog watchdog;
 LowPowerClass lowPowerBoard;
 
 void setup()
 {
-   // watchdog.enable(Watchdog::TIMEOUT_2S);
-    pinMode(LED, OUTPUT);
-    // pinMode(LDR_CONNECT, OUTPUT);
-    // pinMode(NTC_CONNECT, OUTPUT);
-    // Serial.begin(9600); 
-    //===============
-    /*setup FFT Sampling*/
-    sampling_period_us = round(1000000*(1.0/samplingFrequency));
-  
+  // watchdog.enable(Watchdog::TIMEOUT_2S);
+  pinMode(LED, OUTPUT);
+  pinMode(ADC_LINE_CONNECT,OUTPUT);
+#if READ_LDR
+  pinMode(LDR_CONNECT, OUTPUT);
+#endif
+
+#if READ_TEMPERATURE
+  pinMode(NTC_CONNECT, OUTPUT);
+#endif
+Serial.begin(9600);
+#if DEBUG
+  Serial.begin(9600);
+#endif
+  //===============
+
+#if FFT_PIR
+  /*setup FFT Sampling*/
+  sampling_period_us = round(1000000 * (1.0 / samplingFrequency));
+#endif
 }
 String LastPIR;
 void loop()
 {
-// if(!firstTurnON){
 
-//   // while (1)
-//   // {
-//   //   //randomSeed(analogRead(A6));
+  // if(!firstTurnON){
+  //   // while (1)
+  //   // {
+  //   //   //randomSeed(analogRead(A6));
+  //   //  long randDelay=randomSeed(analogRead(A6));
+  //   //   delay(randDelay);
+  //   //   break;
+  //   // }
+  // }
+  // firstTurnON=true;
+  // digitalWrite(LED, LOW);
 
-//   //  long randDelay=randomSeed(analogRead(A6));
-//   //   delay(randDelay);
-//   //   break;
-//   // }
+  lowPowerBoard.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);
+  
+  digitalWrite(ADC_LINE_CONNECT, LOW);
 
-// }
-    firstTurnON=true;
-    //  digitalWrite(LED, LOW);
-    lowPowerBoard.powerDown(SLEEP_2S,ADC_OFF, BOD_OFF);
-    // watchdog.tripped();
-    //  while (1)
-    //  {
-    // double celsius = temperature(analogRead(ADC_NTC),5);
-    // double VLDR = CALCULATE_VLDR(analogRead(ADC_LDR));
-    // double RLDR = CALCULATE_RLDR(VLDR,10000);
-    // double LUX= LUX_CALC_SCALAR * pow(RLDR, LUX_CALC_EXPONENT);
-    // int PIR =analogRead(ADC_PIR);
-    // double PIR;
-    int IR =analogRead(ADC_IR);
-    // digitalWrite(LDR_CONNECT, LOW);
-    // digitalWrite(NTC_CONNECT, LOW);
+  uint32_t R1 = 220;
+  uint32_t R2 = 22;
+  // todo : pin low
+  uint32_t VoltageLine = (R1 + R2)/R2  * analogRead(ADC_LINE)*3 / ADC_10bit;
+  // watchdog.tripped();
+ Serial.print("\n VoltageLine=");
+   Serial.print(VoltageLine);
 
-    /*SAMPLING*/
-    //  microseconds = micros();
-    //   for(int i=0; i<samples; i++)
-    //   {
-    //       vReal[i] = analogRead(CHANNEL);
-    //       vImag[i] = 0;
-    //       while(micros() - microseconds < sampling_period_us){
-    //         //empty loop
-    //       }
-    //       microseconds += sampling_period_us;
-    //   }
 
-    //     FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);	/* Weigh data */
-    //     FFT.compute(FFTDirection::Forward); /* Compute FFT */
-    //     FFT.complexToMagnitude(); /* Compute magnitudes */
-    //     PIR= FFT.majorPeak();
-    //     delay(100); /* Repeat after delay */
-    //   PIR=PIR*10.00;
-    // if (PIR<0)PIR=0;
-    // if (PIR>2000)PIR=1024;
+#if READ_TEMPERATURE 
+  digitalWrite(NTC_CONNECT, LOW);
+  double celsius = temperature(analogRead(ADC_NTC), 5);
+#endif
+#if READ_LDR
+  digitalWrite(LDR_CONNECT, LOW);
+  double VLDR = CALCULATE_VLDR(analogRead(ADC_LDR));
+  double RLDR = CALCULATE_RLDR(VLDR, 10000);
+  double LUX = LUX_CALC_SCALAR * pow(RLDR, LUX_CALC_EXPONENT);
+#endif
+  // double PIR;
+  unsigned int IR = analogRead(ADC_IR);
+ Serial.print("\n IR=");
+   Serial.print(IR);
+#if READ_PIR
+  int PIR = analogRead(ADC_PIR);
+#endif
 
-    // String SPTR =(String)PIR;
-    // if(SPTR==" NAN")SPTR=LastPIR;
-    // else LastPIR=SPTR;
-
-    // char buffer[3];
-    // dtostrf(PIR, 2, 3, buffer); 
-
-    //     String Data = 
-    //     "PIR="+(String)PIR
-    //     +"  IR="+(String)IR
-    //     +"  LDR="+(String)analogRead(ADC_LDR)
-    //     +"   LUX="+(String)LUX
-    //     +"   Temp="+(String)celsius;
-    //    Serial.println(Data);
-    // Serial.print( "PIR="+SPTR);
-    // Serial.print( "PIR="+PIR);
-    // Serial.print("  IR="+(String)IR);
-    // Serial.print("  LDR="+(String)analogRead(ADC_LDR));
-    // Serial.print("  LUX="+(String)LUX);
-    // Serial.print("  Temp="+(String)celsius);
-    // Serial.print("\n");
-// while (1)
-// {
-  BlinkerTimer++;
-  if(BlinkerTimer>=4){
-    digitalWrite(LED, HIGH);
-    delay(1);
-    digitalWrite(LED, LOW);
-    BlinkerTimer=0;
-  }
- 
-  // delay(200);
-// }
-
-    
-    if (IR < TRESHOLT_IR_FIER){
-      FIER=true;
-      FIERCounter++;
-    }
-    else {
-      FIER = false;
-      if(FIERCounter > 0 ) FIERCounter--;
-    }
-
-    if(FIERCounter>3)
+#if FFT_PIR
+  /*SAMPLING*/
+  microseconds = micros();
+  for (int i = 0; i < samples; i++)
+  {
+    vReal[i] = analogRead(CHANNEL);
+    vImag[i] = 0;
+    while (micros() - microseconds < sampling_period_us)
     {
-      while (1)
-      {
-        LineVoltage=analogRead(ADC_LINE);
-        digitalWrite(LED, HIGH);
+      // empty loop
+    }
+    microseconds += sampling_period_us;
+  }
+  FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward); /* Weigh data */
+  FFT.compute(FFTDirection::Forward);                       /* Compute FFT */
+  FFT.complexToMagnitude();                                 /* Compute magnitudes */
+  PIR = FFT.majorPeak();
+  delay(100); /* Repeat after delay */
+              // char buffer[3];
+              // dtostrf(PIR, 2, 3, buffer);
+#endif
 
+#if DEBUG
+  //     String Data =
+  //     "PIR="+(String)PIR
+  //     +"  IR="+(String)IR
+  //     +"  LDR="+(String)analogRead(ADC_LDR)
+  //     +"   LUX="+(String)LUX
+  //     +"   Temp="+(String)celsius;
+  //    Serial.println(Data);
+  PIR = PIR * 10.00;
+  if (PIR < 0)
+    PIR = 0;
+  if (PIR > 2000)
+    PIR = 1024;
+  Serial.print("PIR=" + PIR);
+  Serial.print("  IR=" + (String)IR);
+  Serial.print("  LDR=" + (String)analogRead(ADC_LDR));
+  Serial.print("  LUX=" + (String)LUX);
+  Serial.print("  Temp=" + (String)celsius);
+  Serial.print("\n");
+#endif
+
+
+#if POWER_DISABLE
+  power_adc_disable();
+  power_spi_disable();
+  power_timer0_disable();
+  power_timer1_disable();
+  power_usart0_disable();
+#endif
+
+  if (VoltageLine < MINIMUM_VOLTAGE_LINE_VALID)
+  {
+    digitalWrite(LED, LOW);
+  }
+  else
+  {
+
+    BlinkerTimer++;
+    if (BlinkerTimer >= 3)
+    {
+      digitalWrite(LED, HIGH);
+      _delay_us(100);
+      digitalWrite(LED, LOW);
+      BlinkerTimer = 0;
+    }
+  }
+  if (IR < TRESHOLT_IR_FIER)
+  {
+    FIER = true;
+    FIERCounter++;
+  }
+  else
+  {
+    FIER = false;
+    if (FIERCounter > 0)
+      FIERCounter--;
+  }
+
+  if (FIERCounter > 3)
+  {
+    while (1)
+    {
+       VoltageLine = (R1 + R2)/R2  * analogRead(ADC_LINE)*3 / ADC_10bit;
+
+      if (VoltageLine < MINIMUM_VOLTAGE_LINE_VALID)
+      {
+        digitalWrite(LED, LOW);
+        FIERCounter = 0;
+        FIER=false;
+        break;
+      }
+      else
+      {
+        digitalWrite(LED, HIGH);
       }
     }
-     delay(100);
-    // if(micros() - microseconds < 200)break;
-    // lowPowerBoard.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
-//  }
- 
-   
-
-
+  }
+  delay(100);
 }
--- src/main.hpp
+++ src/main.hpp
@@ -5,30 +5,42 @@
 #include "arduinoFFT.h"
 #include "time.h"
 #include "Watchdog.h"
+// ADC Chanels
 #define ADC_LINE A0
 #define ADC_IR A3
 #define ADC_PIR A4
 #define ADC_LDR PC5
 #define ADC_NTC A2
+
+// 
 #define LDR_CONNECT PD5
 #define NTC_CONNECT PD4
-#define SENSOR_PIN             ADC_NTC
-#define REFERENCE_RESISTANCE   10000
-#define NOMINAL_RESISTANCE     10000
-#define NOMINAL_TEMPERATURE    25
-#define B_VALUE                3950
-#define ANALOG_RESOLUTION      1024
-#define LED  8
-#define CALCULATE_VLDR(ADCLDR)(ADCLDR/(float)1023 )*5
-#define CALCULATE_RLDR(VLDR, ResPullDown) ((5/VLDR-1)*10000)
+#define  ADC_LINE_CONNECT A1
 
+
+
+
+#define LED  8
+#define currentPin
+
+#define CALCULATE_VLDR(ADCLDR)(ADCLDR/(float)ADC_10bit )*5
+#define CALCULATE_RLDR(VLDR, ResPullDown) ((5/VLDR-1)*10000)
 #define CHANNEL ADC_PIR
+
+#define Enable 1
+#define Disable 0
+#define READ_TEMPERATURE    Disable
+#define READ_LDR            Disable
+#define READ_PIR            Disable
+#define FFT_PIR             Disable
+#define POWER_DISABLE       Disable
+#define DEBUG               Disable
 
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #define TRESHOLT_IR_FIER 10
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#define MINIMUM_VOLTAGE_LINE_VALID 9
 
-unsigned int LineVoltage=0;
 
 
 /*=====================================================================*/
