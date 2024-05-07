53234f44e0ba7bc45144e07bf379228271b2493d
mohammad <94738811+Mohamadkhosravi@users.noreply.github.com>
2024-05-07 13:46:40 +0330

    refactor code ,delete extra line ...

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ Watchdog watchdog;
 LowPowerClass lowPowerBoard;
 
 uint32_t readVoltageLine(uint32_t ADCValue);
-uint32_t averageFilter(uint32_t numberOfPin,uint16_t sicleOfSampling, uint16_t ADCOffset );
+uint32_t averageFilter(uint32_t numberOfPin,uint16_t samplingScale, uint16_t ADCOffset );
 
 void setup()
 {
@@ -40,8 +40,6 @@ void setup()
  firstTurnON=false;
  randomSeed(analogRead(A6));
   EEPROM.begin();
-  // EEPROM.write(0,0);
-  // EEPROM.write(20,0);
   calibrationState=bool(EEPROM.read(0));
   if(calibrationState)IROffset=EEPROM.read(20);
 
@@ -69,7 +67,6 @@ void loop()
  
       if(!digitalRead(CALIBRATION_PIN))
       {
-          // delay(10);
         if(!(digitalRead(CALIBRATION_PIN)))
         {  
 
@@ -98,8 +95,7 @@ void loop()
         
     }
     
-    }
- //todo : check firist on turn on current...
+   }
      uint8_t randDelay= random(33,1000);
      if(randDelay>=1000)randDelay=int(randDelay/2);
      if(randDelay>=60000)randDelay=int(randDelay/10);
@@ -111,52 +107,38 @@ void loop()
 
 
   }
+  
   firstTurnON=true;
   digitalWrite(LED, LOW);
   lowPowerBoard.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);  
-  
-  // #define numberOFSamplae
-    uint32_t VoltageLine = readVoltageLine(analogRead(ADC_LINE));
-#if READ_TEMPERATURE 
-  digitalWrite(NTC_CONNECT, LOW);
-  double celsius = temperature(analogRead(ADC_NTC), 5);
-#endif
-#if READ_LDR
-  digitalWrite(LDR_CONNECT, LOW);
-  double VLDR = CALCULATE_VLDR(analogRead(ADC_LDR));
-  double RLDR = CALCULATE_RLDR(VLDR, 10000);
-  double LUX = LUX_CALC_SCALAR * pow(RLDR, LUX_CALC_EXPONENT);
-#endif
+  uint32_t VoltageLine = readVoltageLine(analogRead(ADC_LINE));
 
-unsigned int IR = averageFilter(ADC_IR,5,IROffset);
 
 #if READ_PIR
   int PIR = analogRead(ADC_PIR);
-#endif
 
-#if FFT_PIR
-  /*SAMPLING*/
-  microseconds = micros();
-  for (int i = 0; i < samples; i++)
-  {
-    vReal[i] = analogRead(CHANNEL);
-    vImag[i] = 0;
-    while (micros() - microseconds < sampling_period_us)
+  #if FFT_PIR
+    /*SAMPLING*/
+    microseconds = micros();
+    for (int i = 0; i < samples; i++)
     {
-      // empty loop
+      vReal[i] = analogRead(CHANNEL);
+      vImag[i] = 0;
+      while (micros() - microseconds < sampling_period_us)
+      {
+        // empty loop
+      }
+      microseconds += sampling_period_us;
     }
-    microseconds += sampling_period_us;
-  }
-  FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward); /* Weigh data */
-  FFT.compute(FFTDirection::Forward);                       /* Compute FFT */
-  FFT.complexToMagnitude();                                 /* Compute magnitudes */
-  PIR = FFT.majorPeak();
-  delay(100); /* Repeat after delay */
-              // char buffer[3];
-              // dtostrf(PIR, 2, 3, buffer);
-#endif
+    FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward); /* Weigh data */
+    FFT.compute(FFTDirection::Forward);                       /* Compute FFT */
+    FFT.complexToMagnitude();                                 /* Compute magnitudes */
+    PIR = FFT.majorPeak();
+    delay(100); /* Repeat after delay */
+                // char buffer[3];
+                // dtostrf(PIR, 2, 3, buffer);
 
-#if READ_PIR
+  #endif
   PIR = PIR * 10.00;
   if (PIR < 0)
     PIR = 0;
@@ -164,15 +146,25 @@ unsigned int IR = averageFilter(ADC_IR,5,IROffset);
     PIR = 1024;
   log("PIR=" + PIR);
 #endif
+
 #if READ_IR
+  unsigned int IR = averageFilter(ADC_IR,5,IROffset);
   log("IR=");
   log(IR);
 #endif
+
 #if READ_LDR
+  digitalWrite(LDR_CONNECT, LOW);
+  double VLDR = CALCULATE_VLDR(analogRead(ADC_LDR));
+  double RLDR = CALCULATE_RLDR(VLDR, 10000);
+  double LUX = LUX_CALC_SCALAR * pow(RLDR, LUX_CALC_EXPONENT);
   log("  LDR=" + (String)analogRead(ADC_LDR));
   log("  LUX=" + (String)LUX);
 #endif
+
 #if READ_TEMPERATURE
+  digitalWrite(NTC_CONNECT, LOW);
+  double celsius = temperature(analogRead(ADC_NTC), 5);
   log("  Temp=" + (String)celsius +"\n");
 #endif
 
@@ -210,7 +202,6 @@ unsigned int IR = averageFilter(ADC_IR,5,IROffset);
     while (1)
     {
       // watchdog.tripped();
-       
        VoltageLine = readVoltageLine(analogRead(ADC_LINE));
       if (VoltageLine < MINIMUM_VOLTAGE_LINE_VALID)
       {
@@ -228,33 +219,32 @@ unsigned int IR = averageFilter(ADC_IR,5,IROffset);
   // delay(100);
 }
 
- uint32_t readVoltageLine(uint32_t ADCValue)
- {
-  uint32_t R1 = 220;
-  uint32_t R2 = 22;
-  uint16_t VCC = 3;
-  digitalWrite(ADC_LINE_CONNECT, LOW);
-  uint32_t voltage=(R1 + R2)/R2  * ADCValue * VCC / ADC_10bit;
-  // log("\n VoltageLine"+(String)voltage);
-  return voltage;
- }
-
- uint32_t averageFilter(uint32_t numberOfPin,uint16_t sicleOfSampling, uint16_t ADCOffset )
+  uint32_t readVoltageLine(uint32_t ADCValue)
   {
-    //  std::vector<uint16_t> sicleOfSampling[];
-    
+    uint32_t R1 = 220;
+    uint32_t R2 = 22;
+    uint16_t VCC = 3;
+    digitalWrite(ADC_LINE_CONNECT, LOW);
+    uint32_t voltage=(R1 + R2)/R2  * ADCValue * VCC / ADC_10bit;
+    // log("\n VoltageLine"+(String)voltage);
+    return voltage;
+  }
+
+  uint32_t averageFilter(uint32_t numberOfPin,uint16_t samplingScale, uint16_t ADCOffset )
+  {
+    //ToDo: sampling Scale to=> vactor
+    //  std::vector<uint16_t> samplingScale[];
     uint64_t buffer=0;
-    for (uint16_t numberOfAverage = 0; numberOfAverage < sicleOfSampling-1 ; numberOfAverage++)
-    {
-      
-    buffer = buffer +analogRead(numberOfPin);
-    _delay_us(100);
+    for (uint16_t numberOfAverage = 0; numberOfAverage < samplingScale-1 ; numberOfAverage++)
+    { 
+      buffer = buffer +analogRead(numberOfPin);
+      _delay_us(100);
     }
-     uint32_t result =int(buffer/(sicleOfSampling));
-     if(ADCOffset==0){
-       return result;
-     }
-     if(result>ADCOffset) return result-ADCOffset;
-     else return result;
-    
- }
+    uint32_t result =int(buffer/(samplingScale));
+    if(ADCOffset==0){
+      return result;
+    }
+    if(result>ADCOffset) return result-ADCOffset;
+    else return result;
+      
+  }
